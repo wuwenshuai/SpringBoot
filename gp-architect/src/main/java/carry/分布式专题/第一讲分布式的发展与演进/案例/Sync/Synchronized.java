5446cbe17bcc56bf7534b7413b866f17756de22a
Amos <amos.wu@madeforgoods.com.cn>
2020-07-07 11:36:38 +0800

    syn的字节码

---
+++ gp-architect/src/main/java/carry/分布式专题/第一讲分布式的发展与演进/案例/Sync/Synchronized.java
@@ -0,0 +1,16 @@
+package carry.分布式专题.第一讲分布式的发展与演进.案例.Sync;
+
+public class Synchronized {
+
+    // 同步方法
+   public synchronized  void sync() {
+
+   }
+
+   // 同步代码块
+    public void sync1() {
+       synchronized (Synchronized.class) {
+
+       }
+    }
+}
--- gp-architect/src/main/java/carry/分布式服务治理专题/springBoot原理/spi/SpiMain.java
+++ gp-architect/src/main/java/carry/分布式服务治理专题/springBoot原理/spi/SpiMain.java
@@ -1,7 +1,7 @@
 package carry.分布式服务治理专题.springBoot原理.spi;
 
 
-import com.carry.CarryCore;
+// import com.carry.CarryCore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,6 +12,6 @@ public class SpiMain {
     public static void main(String[] args) {
 
         ConfigurableApplicationContext ca= SpringApplication.run(SpiMain.class,args);
-        ca.getBean(CarryCore.class).study();
+       // ca.getBean(CarryCore.class).study();
     }
 }
